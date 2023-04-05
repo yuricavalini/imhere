b2a56e4802fc0793642e8c72e0f05792857a589a
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-04 21:41:58 -0300

    feat(imhere): add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
+import { StatusBar } from 'react-native'
+
 import { Home } from './src/screens/Home'
 
 export default function App(): JSX.Element {
   return (
+    <>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor="transparent"
+        translucent
+      />
     <Home />
+    </>
   )
 }
